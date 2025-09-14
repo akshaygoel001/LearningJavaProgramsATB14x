019b671f5b55c5487857c0f3dc3ea8ec46cc396b
AkshayG <goelakshay001@gmail.com>
2025-09-14 10:05:14 +0530

    printf practice

---
+++ src/ex_03_Java_Basics/Lab001_printf.java
@@ -0,0 +1,18 @@
+package ex_03_Java_Basics;
+
+public class Lab001_printf {
+
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.printf("%d x 1 = %d",n,(n*1));
+        System.out.printf("\n%d x 2 = %d",n,(n*2));
+        System.out.printf("\n%d x 3 = %d",n,(n*3));
+        System.out.printf("\n%d x 4 = %d",n,(n*4));
+        System.out.printf("\n%d x 5 = %d",n,(n*5));
+        System.out.printf("\n%d x 6 = %d",n,(n*6));
+        System.out.printf("\n%d x 7 = %d",n,(n*7));
+        System.out.printf("\n%d x 8 = %d",n,(n*8));
+        System.out.printf("\n%d x 9 = %d",n,(n*9));
+        System.out.printf("\n%d x 10 = %d",n,(n*10));
+    }
+}
