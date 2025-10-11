3ddbf02be224ba232e2f0048dca573af4b5a79eb
AkshayG <goelakshay001@gmail.com>
2025-10-11 11:03:36 +0530

    Task 11 Oct 2025

---
+++ src/task_11_Oct_2025/InvertedRightTrianglePattern.java
@@ -0,0 +1,19 @@
+package task_11_Oct_2025;
+
+public class InvertedRightTrianglePattern {
+    /*  *****
+        ****
+        ***
+        **
+        *      */
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 5 ; i++){
+            for(int j = i ; j < 5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
