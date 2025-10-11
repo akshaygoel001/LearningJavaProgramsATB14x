0e784c6fa6da7086ea6f55c6b4de808d13f068f0
AkshayG <goelakshay001@gmail.com>
2025-10-11 10:09:38 +0530

    Optimized Second maximum number logic

--- src/task_05_Oct_2025/Second_Maximum_Number.java
+++ src/task_05_Oct_2025/Second_Maximum_Number.java
@@ -2,7 +2,7 @@ package task_05_Oct_2025;
 
 public class Second_Maximum_Number {
     public static void main(String[] args) {
-        int[] numbers = {12, 34, 10, 1, 100, 3, 4, 32};
+        int[] numbers = {12, 34, 10, 1, 100, 3, 4, 34};
         int max = Integer.MIN_VALUE;
         int sec_max = Integer.MIN_VALUE;
         for (int i=0;i<numbers.length;i++){
@@ -11,7 +11,7 @@ public class Second_Maximum_Number {
                 max = numbers[i];
             }
             else {
-                if (numbers[i] > sec_max){
+                if (numbers[i] > sec_max && numbers[i] != max){
                     sec_max = numbers[i];
                 }
             }
