39e80950c4bc6d863ebfb0b11abba180e35a0e99
AkshayG <goelakshay001@gmail.com>
2025-10-12 13:47:51 +0530

    Challenge1_12102025

---
+++ src/coding_challenge_12102025/Challenge1.java
@@ -0,0 +1,11 @@
+package coding_challenge_12102025;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        int int_number = 25;
+        double double_number = 3.14d;
+        boolean boolean_value = true;
+        char ch = 'A';
+        System.out.printf("Integer: %d Double: %.2f Boolean: %b Character: %s",int_number,double_number,boolean_value,ch);
+    }
+}
